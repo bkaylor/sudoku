7d05f3f91b30cebfdee09a2357f46068805ea709
Bryan Kaylor <brskaylor@gmail.com>
2017-11-06 12:28:10 -0600

    Init

---
+++ readme.txt
@@ -0,0 +1,2 @@
+A sudoku solver in cpp.
+Takes one input, a filename containing a sudoku puzzle.
---
+++ sudoku.cpp
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Data 
+{
+    char *data;
+    int size;
+};
+
+struct Puzzle 
+{
+    int pos[9][9];
+};
+
+bool isSolved(char *p);
+void initPuzzle(struct Puzzle *p, struct Data *d);
+
+int main(int argc, char *argv[])
+{
+    struct Data d = {0};
+    struct Puzzle p = {0};
+
+    if (argc != 2) 
+    { 
+        fprintf(stderr, "Error, bad argc. Expected 1, got %d.\n", argc-1);
+        fprintf(stderr, "Usage: solve <filename>\n");
+        exit(1);
+    } 
+    else 
+    {
+        FILE *file = fopen(argv[1], "r");
+
+        if (file) 
+        {
+            fseek(file, 0, SEEK_END);
+            d.size = ftell(file);
+            fseek(file, 0, SEEK_SET);
+
+            d.data = (char *) malloc(d.size+1);
+            fread(d.data, 1, d.size, file);
+            d.data[d.size] = 0;
+
+            fclose(file);
+        }
+    }
+
+    initPuzzle(&p, &d);
+
+    return 0;
+}
+
+void initPuzzle(struct Puzzle *p, struct Data *d)
+{
+    if (d->size != 90) 
+    {
+        fprintf(stderr, "Error, bad file.\n");
+        fprintf(stderr, "Expected size 90, got %d.\n", d->size);
+        exit(1);
+    } 
+    else 
+    {
+        for (int i = 0; i < 9; ++i)
+        {
+            for (int j = 0; j < 9; ++j)
+            {
+                p->pos[i][j] = d->data[i+j];
+            }
+        }
+    }
+}
